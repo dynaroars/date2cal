552e738dd183bc40c1e756ada495299ef83e5f1a
ThanhVu Nguyen <tvn@fedora>
2026-09-19 00:35:43 -0400

    fix(scripts): create zip output directory before building plugin

--- scripts/zip_to_publish.sh
+++ scripts/zip_to_publish.sh
@@ -5,6 +5,8 @@ PLUGIN_VERSION=`jq -r ".version" < manifest.json`
 PLUGIN_VERSION="${PLUGIN_VERSION//./-}"
 PLUGIN_ZIP_NAME="zip/date2cal-${PLUGIN_VERSION}.zip"
 
+mkdir -p zip
+
 if [ -f "$PLUGIN_ZIP_NAME" ]; then
     rm "$PLUGIN_ZIP_NAME"
 fi
